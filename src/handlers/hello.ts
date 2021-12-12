2fe16d408f5172673337a38a9881ed2bd06b5737
John Livingston <git@john-livingston.fr>
2021-12-12 16:19:46 +0100

    Hello: delay parameter.

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -2,21 +2,46 @@ import type { Room } from '../room'
 import type { RoomUser } from '../user'
 import { Handler } from './abstract'
 
+/**
+ * HandlerHello: says hello to incoming users.
+ */
 class HandlerHello extends Handler {
+  protected readonly lastHellos: Map<string, Date> = new Map()
+
+  /**
+   * @param room the room to handle
+   * @param message optionnal message to say. Use {{NICK}} as a placeholder for the user's nick
+   * @param delay if not undefined, do not repeat hello message if the user was already welcomed this past delay seconds
+   */
   constructor (
     room: Room,
-    protected readonly message: string = 'Hello {{NICK}}!'
+    protected readonly message: string = 'Hello {{NICK}}!',
+    protected readonly delay: number | undefined = undefined
   ) {
     super(room)
   }
 
   public start (): this {
     this.on('room_joined', (user: RoomUser) => {
-      if (!user.isMe) {
-        const message = this.message.replace(/{{NICK}}/g, user.nick)
-        // TODO: highlight the user (see XMPP specification)
-        this.room.sendGroupchat(message).catch((err) => { this.logger.error(err) })
+      if (user.isMe) {
+        return
       }
+      if (!this.room.isOnline()) {
+        // must skip if !isOnline, to ignore initials presence messages
+        return
+      }
+      if (this.delay !== undefined) {
+        const lastHello = this.lastHellos.get(user.JID.toString())
+        if (lastHello) {
+          const now = new Date()
+          if ((now.getTime() - lastHello.getTime()) < this.delay) {
+            return
+          }
+        }
+      }
+      const message = this.message.replace(/{{NICK}}/g, user.nick)
+      // TODO: highlight the user (see XMPP specification)
+      this.room.sendGroupchat(message).catch((err) => { this.logger.error(err) })
     })
     return this
   }
